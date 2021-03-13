da0742fe4a287328abde0769e9ec9e6c300bbedd
miyuiki <a121406@gmail.com>
2021-03-13 23:30:01 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+using System;
+using System.Security;
+using System.Windows.Forms;
+
+namespace RMSTSTool
+{
+    partial class Form1:Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private TextBox textBox1;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LogFilePath = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Enabled = true;
+            this.textBox1.Location = new System.Drawing.Point(104, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Location = new System.Drawing.Point(246, 48);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectFileBtn.TabIndex = 1;
+            this.selectFileBtn.Text = "選擇檔案";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            this.selectFileBtn.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Recipe name";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OpenForm);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.LogFilePath);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.selectFileBtn);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(335, 128);
+            this.panel1.TabIndex = 5;
+            // 
+            // LogFilePath
+            // 
+            this.LogFilePath.AutoSize = true;
+            this.LogFilePath.Location = new System.Drawing.Point(8, 52);
+            this.LogFilePath.Name = "LogFilePath";
+            this.LogFilePath.Size = new System.Drawing.Size(78, 15);
+            this.LogFilePath.TabIndex = 5;
+            this.LogFilePath.Text = "Log file path";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 527);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "RMS TS Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void OpenForm(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialog1.FileName;
+                    //using (Stream str = openFileDialog1.OpenFile())
+                    //{
+                    //    Process.Start("notepad.exe", filePath);
+                    //}
+                    this.textBox2.Text = filePath;
+                    //this.label2.Visible = true;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+
+        #endregion
+
+        private Button selectFileBtn;
+        private Label label1;
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private Panel panel1;
+        private Label LogFilePath;
+        private TextBox textBox2;
+    }
+}
+
